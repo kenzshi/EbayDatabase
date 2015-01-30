4530cb179c5bee51bfdd8875740637983d97206d
Daniel Duan <danielduan88@yahoo.com>
2015-01-29 18:38:33 -0800

    finished queries

--- queries.sql
+++ queries.sql
@@ -4,3 +4,4 @@ SELECT COUNT (*) FROM (SELECT COUNT(*) FROM item_category GROUP BY item_id HAVIN
 SELECT id FROM item WHERE ends > '"2001-12-20 00:00:01"' AND Started < "2001-12-20 00:00:00" AND num_bids > 0 ORDER BY currently DESC LIMIT 1;
 SELECT COUNT(*) FROM user WHERE EXISTS(SELECT * FROM item WHERE item.seller_id = user.id) AND user.rating > 1000;
 SELECT COUNT(*) FROM user WHERE EXISTS(SELECT * FROM item WHERE item.seller_id = user.id) AND EXISTS(SELECT * FROM bid WHERE bid.bidder_id = user.id);
+SELECT COUNT(DISTINCT category_id) FROM bid JOIN item_category ON item_category.item_id = bid.item_id WHERE price > 100;
