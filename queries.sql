db72d23cac9e18db1c1c88db0916b00ad5f94f54
Daniel Duan <danielduan88@yahoo.com>
2015-01-29 18:37:12 -0800

    third query

--- queries.sql
+++ queries.sql
@@ -1,6 +1,6 @@
 SELECT COUNT(*) FROM user;
 SELECT COUNT(*) FROM item WHERE BINARY location = 'New York';
-SELECT count(*) FROM item_category GROUP BY item_id HAVING COUNT(category_id) = 4;
+SELECT COUNT (*) FROM (SELECT COUNT(*) FROM item_category GROUP BY item_id HAVING COUNT(category_id) = 4);
 SELECT id FROM item WHERE ends > '"2001-12-20 00:00:01"' AND Started < "2001-12-20 00:00:00" AND num_bids > 0 ORDER BY currently DESC LIMIT 1;
 SELECT COUNT(*) FROM user WHERE EXISTS(SELECT * FROM item WHERE item.seller_id = user.id) AND user.rating > 1000;
 SELECT COUNT(*) FROM user WHERE EXISTS(SELECT * FROM item WHERE item.seller_id = user.id) AND EXISTS(SELECT * FROM bid WHERE bid.bidder_id = user.id);
