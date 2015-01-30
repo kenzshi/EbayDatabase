5ea337c98b19670c57544bd76451cb4f71c2bde3
Kenny Shi <kenshi@ucla.edu>
2015-01-29 17:24:23 -0800

    item-category still not working

--- src/edu/ucla/cs/cs144/MyParser.java
+++ src/edu/ucla/cs/cs144/MyParser.java
@@ -123,6 +123,56 @@ class MyParser {
                 item_seller_id = seller_id;
         }
 
+        @Override
+        public boolean equals(Object obj){
+            return (((Items)obj).item_id).equals(this.item_id);
+        }
+
+        @Override 
+        public int hashCode() { 
+            return item_id.hashCode();
+        }
+
+    }
+
+    public static class Category { 
+        String cat_id;
+        String cat_name;
+        Category(String id, String name){
+            cat_id = id;
+            cat_name = name;
+        }
+
+        @Override
+        public boolean equals(Object obj){
+            return (((Category)obj).cat_name).equals(this.cat_name);
+        }
+
+        @Override 
+        public int hashCode() { 
+            return cat_name.hashCode();
+        }
+    }
+
+
+    public static class Item_Category { 
+        String icat_item_id;
+        String icat_cat_id;
+
+        Item_Category(String item_id, String cat_id){
+            icat_item_id = item_id;
+            icat_cat_id = cat_id;
+        }
+
+        /*@Override
+        public boolean equals(Object obj){
+            return (((Item_Category)obj).icat_item_id + icat_cat_id).equals(this.icat_item_id + this.icat_cat_id);
+        }
+
+        @Override 
+        public int hashCode() { 
+            return (icat_item_id + icat_cat_id).hashCode();
+        }*/
     }
 
     public static class Users { 
@@ -144,7 +194,6 @@ class MyParser {
             return (((Users)obj).user_name).equals(this.user_name);
         }
 
-        //Overrides the hashcode function
         @Override 
         public int hashCode() { 
             return user_name.hashCode();
@@ -153,7 +202,7 @@ class MyParser {
 
 
     //Set for the category, using a set to get rid of duplicates
-    static Set<String> set_category = new HashSet<String>();
+    //static Set<String> set_category = new HashSet<String>();
     //static Map<String,Integer> map_category = new HashMap<String,Integer>();
 
     //Set for Users
@@ -162,8 +211,14 @@ class MyParser {
     //Set of Items
     static Set<Items> set_items = new HashSet<Items>();
 
+    //Set of Categories
+    static Set<Category> set_category = new HashSet<Category>();
+
+    //Set of Item_Categories
+    static Set<Item_Category> set_item_category = new HashSet<Item_Category>();
+
     //Hashmap for file-category
-    static Map<Integer,Integer> map_filecategory = new HashMap<Integer,Integer>(); 
+    //static Map<Integer,Integer> map_filecategory = new HashMap<Integer,Integer>(); 
 
 
     /**************************************************************/
@@ -279,7 +334,6 @@ class MyParser {
         //********** ITERATING THROUGH XML DOCUMENTS
         //**************************************************************/
 
-        try { //Try-catch for FileWriter
 
         Element[] current = getElementsByTagNameNR(doc.getDocumentElement(), "Item");
 
@@ -319,17 +373,26 @@ class MyParser {
 
             //Get Category information, loop through and set Category.dat file
             Element[] categoryXML = getElementsByTagNameNR(current[i], "Category");
+            String category_id;
 
             for(int cat = 0; cat < categoryXML.length ; cat++){
 
-                String category_name = getElementText(categoryXML[cat]); 
+                String category_name = getElementText(categoryXML[cat]);
+                //Create our own category_id value
+                category_id = String.valueOf(set_category.size() + 1);
 
-                //Add category name to our Category set
-                set_category.add(category_name);
+                //Create new Category with Cat_ID and category name
+                Category c = new Category(
+                            category_id,
+                            category_name); 
+                if (set_category.add(c))
 
-                //Adding category name's hash value with item_id
-                map_filecategory.put(Math.abs(category_name.hashCode()), item_id);
-                //file_category_buffer.append(item_id + " " + columnSeparator + " " + Math.abs(category_name.hashCode()) + "\n");
+
+                //Create new Item Category with item_id and cat_id
+                Item_Category ic = new Item_Category(
+                                  String.valueOf(item_id),
+                                  category_id);
+                set_item_category.add(ic);
 
             }
 
@@ -380,7 +443,7 @@ class MyParser {
         //********** WRITING TO ITEM-CATEGORY.DAT -- NOT WORKING
         //**************************************************************/
 
-        FileWriter file_category = new FileWriter("file-category.dat");
+        /*FileWriter file_category = new FileWriter("file-category.dat");
         BufferedWriter file_category_buffer = new BufferedWriter(file_category);
 
         for (Map.Entry<Integer, Integer> entry: map_filecategory.entrySet()) {
@@ -389,11 +452,7 @@ class MyParser {
 
         //Close file/buffer writer
         file_category_buffer.close();
-        file_category.close();
-
-        } catch (IOException e) {
-            System.out.println("ERROR: FileWriter error");
-        }
+        file_category.close(); */
         
         
         /**************************************************************/
@@ -510,24 +569,18 @@ class MyParser {
         //**************************************************************/
 
         try{
-            FileWriter category = new FileWriter("Category.dat");
+            FileWriter category = new FileWriter("category.dat");
             BufferedWriter category_buffer = new BufferedWriter(category);
 
             //category_id is a value we generate on our own
             //int category_id = 1;
 
-            for (String curr : set_category) {
+            for (Category c : set_category) {
 
                 //Assign categiry ID with positive hash value
-                String entry = String.format("%d " + columnSeparator + " %s\n", 
-                            Math.abs(curr.hashCode()),
-                            curr);
-                category_buffer.append(entry); 
-
-                //map_category.put(curr,category_id);
-
-                //Increase our category ID
-                //category_id++;
+                category_buffer.append(String.format("%s " + columnSeparator + " %s\n", 
+                            c.cat_id,
+                            c.cat_name));
             }
 
         //Close file/buffer writer
@@ -537,6 +590,32 @@ class MyParser {
         } catch (IOException e) {
             System.out.println("ERROR: FileWriter error");
         }
+
+        //**************************************************************/
+        //********** WRITING TO ITEM_CATEGORY.DAT 
+        //**************************************************************/
+
+        try{
+            FileWriter item_category = new FileWriter("item_category.dat");
+            BufferedWriter item_category_buffer = new BufferedWriter(item_category);
+
+            for (Item_Category ic : set_item_category) {
+
+                //Add item_id and category_id
+                item_category_buffer.append(String.format("%s " + columnSeparator + " %s\n", 
+                            ic.icat_item_id,
+                            ic.icat_cat_id));
+            }
+
+        //Close file/buffer writer
+        item_category_buffer.close();
+        item_category.close();
+
+        } catch (IOException e) {
+            System.out.println("ERROR: FileWriter error");
+        }
+
+
 
         //**************************************************************/
         //********** WRITING TO USERS.DAT 
@@ -552,7 +631,8 @@ class MyParser {
                     u.user_rating,
                     u.user_location,
                     u.user_country));
-        }
+            }
+
         //Close file/buffer writer
         users_buffer.close();
         users.close();
