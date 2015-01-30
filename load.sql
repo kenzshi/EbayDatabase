79d4ef81e8de479ff71e2c9a8f754628b7e5ab5f
Daniel Duan <danielduan88@yahoo.com>
2015-01-29 16:48:19 -0800

    adding users and items

--- load.sql
+++ load.sql
@@ -3,3 +3,18 @@ INTO TABLE `category`
   FIELDS TERMINATED BY ' |*| '
   LINES TERMINATED BY "\n"
   (id, name);
+
+LOAD DATA LOCAL INFILE 'users.dat'
+INTO TABLE `user`
+  FIELDS TERMINATED BY ' |*| '
+  LINES TERMINATED BY "\n"
+  (id, rating, location, country);
+
+LOAD DATA LOCAL INFILE 'items.dat'
+INTO TABLE `item`
+  FIELDS TERMINATED BY ' |*| '
+  LINES TERMINATED BY "\n"
+  (id, name, currently, buy_price,
+  first_bid, num_bids, location, country,
+  lon, lat, started, ends, description,
+  seller_id);
