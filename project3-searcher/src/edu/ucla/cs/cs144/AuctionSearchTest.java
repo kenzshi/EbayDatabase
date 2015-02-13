4e027aaa17d2d3348a5a740b260e136d444c92e3
Daniel Duan <danielduan88@yahoo.com>
2015-02-12 23:29:01 -0800

    spatial done

--- project3-searcher/src/edu/ucla/cs/cs144/AuctionSearchTest.java
+++ project3-searcher/src/edu/ucla/cs/cs144/AuctionSearchTest.java
@@ -31,19 +31,19 @@ public class AuctionSearchTest {
 		System.out.println("Basic Search Query: " + query3);
 		System.out.println("Received " + basicResults3.length + " results");
 
-
-		for(SearchResult result : basicResults3) {
-			System.out.println(result.getItemId() + ": " + result.getName());
-		}
+		//
+		// for(SearchResult result : basicResults3) {
+		// 	System.out.println(result.getItemId() + ": " + result.getName());
+		// }
 
 		SearchRegion region =
 		    new SearchRegion(33.774, -118.63, 34.201, -117.38);
 		SearchResult[] spatialResults = as.spatialSearch("camera", region, 0, 20);
 		System.out.println("Spatial Seacrh");
 		System.out.println("Received " + spatialResults.length + " results");
-		for(SearchResult result : spatialResults) {
-			System.out.println(result.getItemId() + ": " + result.getName());
-		}
+		// for(SearchResult result : spatialResults) {
+		// 	System.out.println(result.getItemId() + ": " + result.getName());
+		// }
 		//1497595357 Original value
 		//1045678297 Needs Description to escape
 		String itemId = "1043817906";
