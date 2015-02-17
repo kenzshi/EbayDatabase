0fb3bae757f732494b733986b85756a98fd8f21a
Kenny Shi <kenshi@ucla.edu>
2015-02-17 00:36:57 -0800

    project 4 starter file

--- project3-indexer/buildSQLIndex.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE spatial_item LIKE item;
-ALTER TABLE spatial_item ENGINE=MYISAM;
-INSERT INTO spatial_item SELECT * FROM item;
-ALTER TABLE spatial_item ADD COLUMN pt POINT;
-UPDATE spatial_item SET pt=Point(lat, lon);
-ALTER TABLE spatial_item MODIFY pt POINT NOT NULL;
-CREATE spatial INDEX ix_spatial_item_pt ON spatial_item(pt);
--- project3-searcher/README.txt
+++
@@ -1,3 +0,0 @@
-ALL README COMMENTS ARE IN project3-indexer
-
-
